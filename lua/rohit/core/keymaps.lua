4b2cb9e53c51f2f0df7006c8d1be3148107e18d6
Rohit Darekar <rohit.darekar@aitglobalinc.com>
2025-11-21 17:58:17 +0530

    added keybinding for text selection and copy past support via normal keys

--- lua/rohit/core/keymaps.lua
+++ lua/rohit/core/keymaps.lua
@@ -30,3 +30,18 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- visual mode text selection with shift+arrows
+keymap.set("n", "<S-Up>", "v<Up>", { desc = "Start visual selection up" })
+keymap.set("n", "<S-Down>", "v<Down>", { desc = "Start visual selection down" })
+keymap.set("n", "<S-Left>", "v<Left>", { desc = "Start visual selection left" })
+keymap.set("n", "<S-Right>", "v<Right>", { desc = "Start visual selection right" })
+keymap.set("v", "<S-Up>", "<Up>", { desc = "Extend selection up" })
+keymap.set("v", "<S-Down>", "<Down>", { desc = "Extend selection down" })
+keymap.set("v", "<S-Left>", "<Left>", { desc = "Extend selection left" })
+keymap.set("v", "<S-Right>", "<Right>", { desc = "Extend selection right" })
+
+-- copy and paste with ctrl+c and ctrl+v
+keymap.set("v", "<C-c>", '"+y', { desc = "Copy to system clipboard" })
+keymap.set({"n", "v"}, "<C-v>", '"+p', { desc = "Paste from system clipboard" })
+keymap.set("i", "<C-v>", '<C-r>+', { desc = "Paste from system clipboard in insert mode" })
